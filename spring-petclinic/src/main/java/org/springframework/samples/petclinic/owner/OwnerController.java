4de659a89bae2f8567c41202e58096b75ceaf628
yoogle96 <dmltjs851@gmail.com>
2020-03-10 02:23:41 +0900

    Update OwnerController dependency injection

--- spring-petclinic/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ spring-petclinic/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -45,9 +45,12 @@ class OwnerController {
 
 	private VisitRepository visits;
 
-	public OwnerController(OwnerRepository clinicService, VisitRepository visits) {
+	private PetRepository petRepository;
+
+	public OwnerController(OwnerRepository clinicService, VisitRepository visits, PetRepository petRepository) {
 		this.owners = clinicService;
 		this.visits = visits;
+		this.petRepository = petRepository;
 	}
 
 	@InitBinder
