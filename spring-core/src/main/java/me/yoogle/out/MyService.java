e62341f56279d7f9d9e86c89a3dfecf1d49b3bf6
yoogle96 <dmltjs851@gmail.com>
2020-03-10 23:06:22 +0900

    Complete IoC 컨테이너 : @Component와 컴포넌트 스캔

---
+++ spring-core/src/main/java/me/yoogle/out/MyService.java
@@ -0,0 +1,7 @@
+package me.yoogle.out;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
--- spring-core/src/main/java/me/yoogle/springcore/SpringCoreApplication.java
+++ spring-core/src/main/java/me/yoogle/springcore/SpringCoreApplication.java
@@ -1,13 +1,27 @@
 package me.yoogle.springcore;
 
+import me.yoogle.out.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreApplication {
 
+	@Autowired
+	MyService myService;
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+		new SpringApplicationBuilder()
+				.sources(SpringCoreApplication.class).
+				initializers((ApplicationContextInitializer<GenericApplicationContext>) applicationContext -> {
+					applicationContext.registerBean(MyService.class);
+				}).run(args);
+
+//		SpringApplication.run(SpringCoreApplication.class, args);
 
 	}
 
