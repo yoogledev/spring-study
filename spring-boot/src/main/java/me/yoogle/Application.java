ee87c2876678cf6c033dd75139f69543b947f86f
yoogle96 <dmltjs851@gmail.com>
2020-04-27 02:03:32 +0900

    Complete 내장 웹 서버 응용 : HTTPS와 HTTP2

--- spring-boot/src/main/java/me/yoogle/Application.java
+++ spring-boot/src/main/java/me/yoogle/Application.java
@@ -1,12 +1,18 @@
 package me.yoogle;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
