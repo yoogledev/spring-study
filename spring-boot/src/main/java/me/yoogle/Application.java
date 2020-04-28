388b1256293c637b20cc612f7ddce6c85ba3b0ad
yoogle96 <dmltjs851@gmail.com>
2020-04-28 14:53:16 +0900

    Complete SpringApplication 1

--- spring-boot/src/main/java/me/yoogle/Application.java
+++ spring-boot/src/main/java/me/yoogle/Application.java
@@ -2,6 +2,7 @@ package me.yoogle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,8 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .run(args);
     }
 }
