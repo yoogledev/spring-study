0a5aff0d29256f1653ab48faed2730f83f86e9a3
yoogle96 <dmltjs851@gmail.com>
2020-05-02 11:41:08 +0900

    Complete 스프링 웹 MVC 2 : HttpMessageConverters

--- spring-boot/src/main/java/me/yoogle/user/UserController.java
+++ spring-boot/src/main/java/me/yoogle/user/UserController.java
@@ -1,6 +1,9 @@
 package me.yoogle.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,8 @@ public class UserController {
         return "hello";
     }
 
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
+    }
 }
--- spring-boot/src/test/java/me/yoogle/UserControllerTest.java
+++ spring-boot/src/test/java/me/yoogle/UserControllerTest.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -24,4 +30,17 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
     }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson = "{\"username\":\"yoogle\", \"password\":\"123\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username", is(equalTo("yoogle"))))
+                .andExpect(jsonPath("$.password", is(equalTo("123"))));
+    }
+
 }
