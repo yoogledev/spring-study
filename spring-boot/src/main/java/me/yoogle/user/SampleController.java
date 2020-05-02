13dad4e1b02a8395f103948aa3e5f8496c553c87
yoogle96 <dmltjs851@gmail.com>
2020-05-02 13:12:42 +0900

    Complete 스프링 웹 MVC 7 : Thymeleaf

---
+++ spring-boot/src/main/java/me/yoogle/user/SampleController.java
@@ -0,0 +1,15 @@
+package me.yoogle.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "yoogle");
+        return "hello";
+    }
+}
---
+++ spring-boot/src/test/java/me/yoogle/user/SampleControllerTest.java
@@ -0,0 +1,42 @@
+package me.yoogle.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+
+    public void hello() throws Exception {
+        // 요청 "/hello"
+        // 응답
+        // - 모델 name : yoogle
+        // - 뷰 이름 : hello
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("yoogle")))
+                .andExpect(content().string(containsString("yoogle")));
+
+    }
+}
