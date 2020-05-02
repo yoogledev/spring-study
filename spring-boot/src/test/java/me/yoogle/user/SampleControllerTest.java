300e9c5cce08767242fce92137ac3a1042cd2afc
yoogle96 <dmltjs851@gmail.com>
2020-05-02 13:44:04 +0900

    Complete 스프링 웹 MVC 8 : HtmlUnit

--- spring-boot/src/test/java/me/yoogle/user/SampleControllerTest.java
+++ spring-boot/src/test/java/me/yoogle/user/SampleControllerTest.java
@@ -1,5 +1,8 @@
 package me.yoogle.user;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,36 +10,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
-
     public void hello() throws Exception {
-        // 요청 "/hello"
-        // 응답
-        // - 모델 name : yoogle
-        // - 뷰 이름 : hello
-
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name", is("yoogle")))
-                .andExpect(content().string(containsString("yoogle")));
-
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("yoogle");
     }
 }
