5424884fbc4f2d378ab582e12d6c0751fcdeee1d
yoogle96 <dmltjs851@gmail.com>
2020-05-02 17:13:18 +0900

    Complete 스프링 웹 MVC 10 : Spring HATEOAS

--- spring-boot/src/main/java/me/yoogle/SampleController.java
+++ spring-boot/src/main/java/me/yoogle/SampleController.java
@@ -1,24 +1,30 @@
 package me.yoogle;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.annotation.Resource;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
 public class SampleController {
 
     @GetMapping("/hello")
-    public String hello() {
-        throw new SampleException();
-    }
+    public EntityModel<Hello> hello() {
+        Hello hello = new Hello();
+        hello.setPrefix("Hey,");
+        hello.setName("yoogle");
 
-    @ExceptionHandler(SampleException.class)
-    public @ResponseBody AppError sampleError(SampleException e) {
-        AppError appError = new AppError();
-        appError.setMessage("error.app.key");
-        appError.setReason("IDK IDK IDK");
-        return appError;
+        EntityModel<Hello> helloEntityModel = new EntityModel<>(hello);
+        helloEntityModel.add(linkTo(methodOn(SampleController.class).hello()).withSelfRel());
+
+        return helloEntityModel;
     }
 }
