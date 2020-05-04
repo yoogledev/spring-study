4d034b04b04e996f8edf0a801c740344290d4daa
yoogle96 <dmltjs851@gmail.com>
2020-05-04 22:40:14 +0900

    Complete 스프링 데이터 2 : 인메모리 데이터베이스

---
+++ spring-boot-jdbc/src/main/java/me/yoogle/springbootjdbc/H2Runner.java
@@ -0,0 +1,35 @@
+package me.yoogle.springbootjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            connection.getMetaData().getURL();
+            connection.getMetaData().getUserName();
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(ID))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'yoogle')");
+    }
+}
---
+++ spring-boot-jdbc/src/main/java/me/yoogle/springbootjdbc/SpringBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package me.yoogle.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcApplication.class, args);
+    }
+
+}
