3c8bbf2bd71f0353655f1654968851eaf869cd95
yoogle96 <dmltjs851@gmail.com>
2020-05-05 00:14:02 +0900

    Complete 스프링 데이터 4 : PostgreSQL

--- spring-boot-jdbc/src/main/java/me/yoogle/springbootjdbc/PgSQLRunner.java
+++ spring-boot-jdbc/src/main/java/me/yoogle/springbootjdbc/PgSQLRunner.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class MySQLRunner implements ApplicationRunner {
+public class PgSQLRunner implements ApplicationRunner {
 
     @Autowired
     DataSource dataSource;
@@ -23,14 +23,15 @@ public class MySQLRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         try(Connection connection = dataSource.getConnection()) {
             System.out.println(connection.getClass());
+            System.out.println(connection.getMetaData().getDriverName());
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
 
-            Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(ID))";
-            statement.executeUpdate(sql);
+//            Statement statement = connection.createStatement();
+//            String sql = "CREATE TABLE ACCOUNT(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(ID))";
+//            statement.executeUpdate(sql);
         }
 
-        jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'yoogle')");
+//        jdbcTemplate.execute("INSERT INTO ACCOUNT VALUES(1, 'yoogle')");
     }
 }
