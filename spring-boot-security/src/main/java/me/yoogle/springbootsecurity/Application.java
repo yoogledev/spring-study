a7a18f5b5a83aecba8ffc9e08f82a6404fafa049
yoogle96 <dmltjs851@gmail.com>
2020-05-06 15:15:14 +0900

    Complete 스프링 시큐리티 1 : Starter-Security

---
+++ spring-boot-security/src/main/java/me/yoogle/springbootsecurity/Application.java
@@ -0,0 +1,12 @@
+package me.yoogle.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring-boot-security/src/main/java/me/yoogle/springbootsecurity/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package me.yoogle.springbootsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
