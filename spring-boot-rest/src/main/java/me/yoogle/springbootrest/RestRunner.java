d8c069bd9efdfb0a55068cfb82a757bea1c83d84
yoogle96 <dmltjs851@gmail.com>
2020-05-06 17:47:30 +0900

    Complete 스프링 REST 클라이언트 1 : RestTemplate과 WebClient

---
+++ spring-boot-rest/src/main/java/me/yoogle/springbootrest/RestRunner.java
@@ -0,0 +1,55 @@
+package me.yoogle.springbootrest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RestRunner implements ApplicationRunner {
+
+    @Autowired
+    WebClient.Builder builder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        WebClient webClient = builder.build();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        helloMono.subscribe(s -> {
+            System.out.println(s);
+
+            if(stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+
+        Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+                .retrieve()
+                .bodyToMono(String.class);
+        worldMono.subscribe(s -> {
+            System.out.println(s);
+
+            if(stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+    }
+}
