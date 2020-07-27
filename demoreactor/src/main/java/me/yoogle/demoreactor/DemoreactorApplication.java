385817ba2c8bd4440f85660b4e5eb9a4c22845fe
Yu UiSeon <dmltjs851@gmail.com>
2020-07-28 00:34:05 +0900

    Complete Reactor : Flux와 Mono

--- demoreactor/src/main/java/me/yoogle/demoreactor/DemoreactorApplication.java
+++ demoreactor/src/main/java/me/yoogle/demoreactor/DemoreactorApplication.java
@@ -2,12 +2,17 @@ package me.yoogle.demoreactor;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 public class DemoreactorApplication {
 
-    public static void main(String[] args) {
-        Flux<String> flux = Flux.just("A");
-        Flux<String> flux2 = flux.map(i -> "foo" + i);
-        flux2.subscribe(System.out::println);
+    public static void main(String[] args) throws Exception {
+        Flux.interval(Duration.ofMillis(100))
+                .take(10)
+                .subscribe(System.out::println);
+
+        System.out.println("여기 먼저??");
+        Thread.sleep(5000);
     }
 
 }
